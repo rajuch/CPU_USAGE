cf841ce9cc85a00df26f97753fc58d2958199eb5
trash-anshu <anshu.choubey.exams@gmail.com>
2014-04-02 18:22:12 +0530

    Implemented using threads and locks. Coding style fixes.

--- cpu_usage/app/record_usage.py
+++ cpu_usage/app/record_usage.py
@@ -1,9 +1,7 @@
-'''Record CPU usage in and fetch them from a file.
+'''Record CPU usage in a concurrent thread.
 
-cpu_usage/temp/store is the file in which the details are stored.
-The CPU usage datas are sampled at the frequency of 1 second. And, the
-average of the usages over 60 seconds is also calculated and stored in
-a queue for every process.
+The CPU usage datas are sampled at the frequency of 1 second. The average of
+the usages over 60 seconds is also calculated and updated for every process.
 
 @created: Mar 21, 2014
 @author: Anshu Kumar, <anshu.choubey@imaginea.com>
@@ -11,19 +9,20 @@ a queue for every process.
 @todo(Implement Logging)
 '''
 
-# pylint: disable=C0103
-
-import json
 import psutil
 import time
 
 from decimal import Decimal
+from threading import Lock
 
-from settings import SAMPLING_FREQ, AVG_INTERVAL, STORE_FILE
+import settings
 
 
-class Queue(object):
-    """Queue data-structure implementation."""
+class PeekableQueue(object):
+    """Queue data-structure implementation. Different from inbuilt queue, in
+    terms that, the elements can be iterated and fetched all at once for some
+    operation, e.g. computing average.
+    """
 
     def __init__(self, maxsize='inf'):
         """Initialise the queue object.
@@ -49,114 +48,156 @@ class Queue(object):
         """
         if self.is_full():
             self.dequeue()
-        self.items.insert(0, item)
+        self.items.append(item)
 
     def dequeue(self):
         """Remove  and return the first in element from the present queue."""
-        return self.items.pop()
+        if self.size():
+            return self.items.pop(0)
 
     def size(self):
         """Calculate and return length of the queue."""
         return len(self.items)
 
 
-# Stores queues for the CPU times of the processes.
-# Format: {'1': {'user_q': user_queue, 'sys_q': sys_queue}}
-process_queue = dict()
-# Stores CPU usage details for the processes.
-# Format: {"1": {"status": "sleeping", "user_avg": 0.3, "name": "init",
-#                "pid": 1, "sys_avg": 0.56,
-#                }}
-processes = dict()
-
-
-def add_processes(avg_interval):
-    """Process CPU usage details for current processes and store them.
-
-    Gets the list of current processes, computes their details and feed
-    them into the class variables so that they can be stored in the file:
-        cpu_usage/temp/store
-    The average of CPU usages over the interval is also calculated and
-    feed in the queue here.
-
-    @param avg_interval: Interval over which average is to be calculated.
-        Used as the length of the queue.
-    @handles StopIteration.
+class RecordUsage(object):
+    """Records CPU usage sampled at a certain rate and averaged over a
+    certain interval.
     """
-    _process = psutil.process_iter()
-    try:
-        while 1:
+    # Stores queues for the CPU times of the processes.
+    # Format: {1: {'user_avg_q': user_queue, 'sys_avg_q': sys_queue},
+    #          }
+    process_queue = dict()
+    # Stores CPU usage details for the processes.
+    # Format: {1: {
+    #              "status": "sleeping", "user_avg": 0.3, "name": "init",
+    #              "pid": 1, "sys_avg": 0.56,
+    #              },
+    #          }
+    processes = dict()
+
+    def __init__(self, sampling_freq=settings.SAMPLING_FREQ,
+                 avg_interval=settings.AVG_INTERVAL, _lock=Lock()):
+        """Initialise class object.
+
+        @param sampling_freq: Time interval in seconds at which the usage
+            is to be sampled. Defaults to 1 second.
+        @param avg_interval: Time interval in seconds over which the average
+            is to be calculated. Defaults to 60 seconds.
+        @param _lock: Thread lock instance.
+        """
+        self.sampling_freq = sampling_freq
+        self.avg_interval = avg_interval
+        # Uses lock while reading and writing dictionaries.
+        self.lock = _lock
+
+    def add_processes(self,):
+        """Process CPU usage details for current processes and store them.
+
+        Gets the list of current processes, computes their details and store
+        them in memory. The details include average of CPU usages over the
+        required time interval.
+        @return: Set of current PIDs. To cull dead processes.
+        """
+        curr_pids = set()
+        for _process in psutil.process_iter():
             try:
-                new_proc = _process.next().\
+                _proc = _process.\
                     as_dict(attrs=['cpu_times', 'name', 'pid', 'status'])
             except psutil.NoSuchProcess:
                 continue
-            pid, (user, _sys) = new_proc['pid'], new_proc.pop('cpu_times')
+            pid, (user, _sys) = _proc['pid'], _proc.pop('cpu_times')
+            curr_pids.add(pid)
+            # Delete ZOMBIE process details.
+            if _proc['status'] in [psutil.STATUS_ZOMBIE]:
+                try:
+                    map(lambda x: x.pop(pid),
+                        [self.processes, self.process_queue])
+                except KeyError:
+                    pass
+                continue
             # Get or create details object for the process.
-            existing = processes.setdefault(pid, new_proc)
+            with self.lock:
+                existing = self.processes.setdefault(pid, _proc)
+            # PID reuse case. Emptying average count queue for the PID.
+            if _proc['name'] != existing['name']:
+                map(lambda x: x.pop(pid), [self.process_queue])
             # Get or create queue object for the CPU times of the process.
-            queue_dict = process_queue.setdefault(pid, dict())
-            # Get or create queue object for the user CPU time.
-            user_q = queue_dict.setdefault('user_q', Queue(avg_interval))
-            # Enqueue user CPU usage value.
-            user_q.enqueue(user)
-            # Calculate current user average.
-            user_avg = get_avg(user_q)
-            # Get or create queue object for the system CPU time.
-            sys_q = queue_dict.setdefault('sys_q', Queue(avg_interval))
-            # Enqueue system CPU usage value.
-            sys_q.enqueue(_sys)
-            # Calculate current system average.
-            sys_avg = get_avg(sys_q)
+            avg_q = self.process_queue.setdefault(pid, dict())
+
+            # User CPU time.
+            user_avg_q = avg_q.setdefault('user_avg_q',
+                                          PeekableQueue(self.avg_interval))
+            user_avg_q.enqueue(user)
+            user_avg = get_avg(user_avg_q)
+
+            # System CPU time.
+            sys_avg_q = avg_q.setdefault('sys_avg_q',
+                                         PeekableQueue(self.avg_interval))
+            sys_avg_q.enqueue(_sys)
+            sys_avg = get_avg(sys_avg_q)
             # Update the details object for the process.
-            existing.update(user_avg=user_avg, sys_avg=sys_avg, **new_proc)
-    except StopIteration:
-        pass
+            with self.lock:
+                existing.update(user_avg=user_avg, sys_avg=sys_avg, **_proc)
+        return curr_pids
+
+    def cull_dead_processes(self, curr_pids):
+        """Cull dead processes.
+
+        @param curr_pids: Set of current PIDs. Cull other PID process.
+        """
+        def cull_dead(process_details):
+            """Return a new dictionary having details for the processes
+            in current PID set."""
+            return {pid: details
+                    for pid, details in process_details.iteritems()
+                    if pid in curr_pids}
+        with self.lock:
+            self.processes = cull_dead(self.processes)
+        self.process_queue = cull_dead(self.process_queue)
+
+    def get_curr_processes(self):
+        """Return details for all current processes.
+        @return: List of current processes' details.
+        """
+        curr = list()
+        for _process in psutil.process_iter():
+            try:
+                _proc = _process.as_dict(attrs=['name', 'pid', 'status'])
+                # Delete process details if gone ZOMBIE.
+                if _proc['status'] in [psutil.STATUS_ZOMBIE]:
+                    continue
+                with self.lock:
+                    details = self.processes[_proc['pid']]
+                # PID reuse case. Emptying average count queue for the PID.
+                if _proc['name'] != details['name']:
+                    continue
+            except (psutil.NoSuchProcess, KeyError):
+                continue
+            curr.append(details)
+        return curr
+
+    def record(self):
+        """Function invoked to record usage at sampling frequency interval."""
+        while True:
+            curr_pids = self.add_processes()
+            self.cull_dead_processes(curr_pids)
+            time.sleep(self.sampling_freq)
 
 
-def get_curr_processes(stored_process):
-    """Return details for all current processes.
-
-    @param stored_process: Stored details for the processes.
-    @return: List of details of all current processes.
-    """
-    return [stored_process[str(pid)] for pid in psutil.get_pid_list()
-            if str(pid) in stored_process]
-
-
-def get_avg(q_obj, round_to=2):
+def get_avg(q_obj, round_to=settings.ROUND_TO):
     """Calculates average of the CPU usage over the interval.
 
     @param q_obj: Queue containing usage for process whose average is to
         be calculated.
-    @param round_to: Number of places after decimal to round upto.
+    @param round_to: Number of places after the decimal to round the value
+        upto. Defaults to 2.
     @return: Calculated average of the CPU usage for the process.
     """
     return round(Decimal(sum(q_obj.items) / q_obj.size()), round_to)
 
 
-def record(sampling_freq=SAMPLING_FREQ, avg_interval=AVG_INTERVAL):
-    """Function invoked to record usage at every sampling frequency interval.
-
-    Stores the processes' details at the interval of sampling frequency in the
-    cpu_usage/temp/store file.
-
-    @param sampling_freq: Time interval in seconds at which usage to be sampled.
-        Defaults to 1 second.
-    @param avg_interval: Time interval in seconds over which the average is to
-        be calculated. Defaults to 60 seconds.
-    """
-    while 1:
-        add_processes(avg_interval)
-        with open(STORE_FILE, 'w') as fp:
-            fp.write(json.dumps(processes))
-        time.sleep(sampling_freq)
-
-
 if __name__ == '__main__':
-    try:
-        record()
-    except KeyboardInterrupt:
-        pass
-    except Exception:
-        raise
+    # pylint: disable=C0103
+    ru_obj = RecordUsage()
+    ru_obj.add_processes()
